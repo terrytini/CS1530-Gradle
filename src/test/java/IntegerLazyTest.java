608f966f019a26cefb7c160674b62bce41a8e7ea
Pauline Walsh <pcw14@pitt.edu>
2016-09-27 16:51:28 -0400

    fixed issues hopefully

---
+++ src/test/java/IntegerLazyTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tan_walsh.IntegerSequence;
+
+public class IntegerLazyTest{
+
+	@Test
+	public void test0(){
+		assertEquals(IntegerSequence.lazy(0), 1);
+	}
+
+	@Test
+	public void test5() {
+		assertEquals(IntegerSequence.lazy(5), 16);
+	}
+
+	@Test
+	public void test10() {
+		assertEquals(IntegerSequence.lazy(10), 56);
+	}
+}
