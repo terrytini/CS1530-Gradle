c34b0275aa5947b29aa1a43fedbb12fb49689725
tet19 <tet19@pitt.edu>
2016-09-27 14:21:23 -0400

    adding integer triangle test

---
+++ src/test/java/IntegerTriangleTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import IntegerSequence;
+
+public class IntegerTriangleTest{
+	@Test
+	public void testTriangle0() {
+		assertEquals(IntegerSequence.tri(0), 0);
+	}
+	
+	@Test
+	public void testTriangle5() {
+		assertEquals(IntegerSequence.tri(5), 15);
+	}
+	
+	@Test
+	public void testTriangle15() {
+		assertEquals(IntegerSequence.tri(15), 120);
+	}
+}
